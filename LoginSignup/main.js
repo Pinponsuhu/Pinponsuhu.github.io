07b36e2c4069d94639d70f586828ba1d84b2e279
Pinponsuhu <Pinponsuhuomorinsola@gmail.com>
2020-10-04 22:58:13 +0100

    initial commit

---
+++ LoginSignup/main.js
@@ -0,0 +1,9 @@
+function sec() {
+  document.getElementById('sec2').style.display = 'none';
+  document.getElementById('sec').style.display = 'block';
+}
+
+function sec2() {
+  document.getElementById('sec').style.display = 'none';
+  document.getElementById('sec2').style.display = 'block';
+}
