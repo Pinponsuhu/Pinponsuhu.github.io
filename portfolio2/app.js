ed0a34bfb0b44a7b02d04e6f1e6203f72981b921
Pinponsuhu <Pinponsuhuomorinsola@gmail.com>
2021-02-18 16:56:11 -0800

    Updated

--- portfolio2/app.js
+++ portfolio2/app.js
@@ -1,51 +1,35 @@
-Particles.init
-({
+
+
+function load() {
+  var a = document.getElementById("searchField").value;
+  var link = "http://www.omdbapi.com/?t=" + a + "&apikey=ea41da5c"
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+
+  fetch(link, requestOptions)
+    .then(response => response.json())
+    .then(function(result) {
+
+        var mainSec = document.getElementById('mainSec');
+        
+        var nImg = document.createElement('img');
+        var nP = document.createElement('p');
+        var nP2 = document.createElement('p');
+        nImg.id = 'newsimg';
+        nP.id = 'titles';
+        nImg.className = 'w-64 h-56 object-fill mx-auto mb-6 mt-12';
+        nP.className = 'text-2xl text-center';
+        nP2.className = 'text-lg text-center px-4';
+        nImg.src = result.Poster;
+        nP.innerHTML = result.Title;
+        nP2.innerHTML = result.Plot;
+        mainSec.appendChild(nImg);
+        mainSec.appendChild(nP);
+        mainSec.appendChild(nP2);
   
-// normal options
-  selector: 
-'.background'
-,
-  maxParticles: 
-450
-,
-  
-// options for breakpoints
-  responsive: [
-    {
-      breakpoint: 
-768
-,
-      options: {
-        maxParticles: 
-200
-,
-        color: 
-'#48F2E3'
-,
-        connectParticles: 
-false
-      }
-    }, {
-      breakpoint: 
-425
-,
-      options: {
-        maxParticles: 
-100
-,
-        connectParticles: 
-true
-      }
-    }, {
-      breakpoint: 
-320
-,
-      options: {
-        maxParticles: 
-0
- 
-// disables particles.js
-      }
-    }
-  ]
-});
+        document.getElementById("resultSec").style.display = "none";
+    })
+    .catch(error => console.log('error', error));
+}
