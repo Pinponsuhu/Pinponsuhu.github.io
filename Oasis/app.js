62259d7a8ff3de575f1f4e2166be76d388d68a4b
Pinponsuhu <Pinponsuhuomorinsola@gmail.com>
2021-03-28 01:04:41 -0700

    Oasis tech blog updated

--- Oasis/app.js
+++ Oasis/app.js
@@ -1,14 +1,5 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
-
-const con = mysql.createConnection({
-    host : 'localhost',
-    user: 'root',
-    password: '',
-    database: 'oasis'
-
-});
 
 const port = process.env.PORT || 3000;
 app.listen(port);
@@ -25,15 +16,4 @@ app.get('/create',(req,res)=>{
 });
 app.get('/about',(req,res)=>{
     res.render('about');
-});
-app.post('/gist', (req,res)=>{
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-        var sql = "INSERT INTO blogPost (title, author,body) VALUES (?,?,?)";
-        con.query(sql,[req.body.blogTitle,req.body.blogAuthor,req.body.blogBody], function (err, result) {
-          if (err) throw err;
-          console.log("1 record inserted");
-        });
-      });
 });
